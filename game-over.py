fd367eff889095340ae911dfca9378a1b87e9f96
Regita Andrianne <62114167+RegitaAndrianne@users.noreply.github.com>
2021-08-19 03:46:33 +0700

    initial commit

--- game-over.py
+++ game-over.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 from random import randint
 
 pygame.init()
@@ -9,7 +9,7 @@ screen_width = 1280
 screen_height = 720
 pygame.display.set_caption('Collect the Trash') #window title
 clock = pygame.time.Clock() 
-FPS = 120
+# FPS = 120
 
 #background
 court = pygame.image.load('images/bg_2.jpg').convert()
@@ -20,22 +20,22 @@ text_y = 15
 font = pygame.font.Font("freesansbold.ttf", 40)
 
 #diver or player
-diver = pygame.image.load('images/diver-up.png').convert_alpha()
-diver_width = 100
-diver_pos_x = 670
-diver_pos_y = 500
-diver_rep_x = [diver_pos_x, diver_pos_x] 
-diver_rep_y = [diver_pos_y, diver_pos_y]
+diver = pygame.image.load('images/diver-down.png').convert_alpha()
+diver_width = 189
+diver_pos_x = 500
+diver_pos_y = 550
+diver_rep_x = [diver_pos_x +31, diver_pos_x+94] 
+diver_rep_y = [diver_pos_y+14, diver_pos_y+20]
 diver_speed = 10  #driver speed movement
 
 #object plastic
 plastic = pygame.image.load('images/plastic.png').convert_alpha()
 x = randint(0, screen_width)
 y = 0
-radius = 100
+radius = 189
 plastic_rep_x = x
 plastic_rep_y = y
-speed = 5 #speed of the plastic fall
+speed = 2 #speed of the plastic fall
 
 play = True
 
@@ -44,7 +44,7 @@ bg_music = pygame.mixer.Sound("sounds/theme-song2.ogg")
 bg_music.play(-1)
 
 #sound effect
-ting = pygame.mixer.Sound("sounds/Ting.wav")
+ting = pygame.mixer.Sound("sounds/cling.wav")
 
 #score
 score = 0
@@ -66,8 +66,8 @@ def show_images():
     global diver_rep_x, diver_rep_y
     screen.blit(plastic, (x,y))
     screen.blit(diver, (diver_pos_x, diver_pos_y))
-    diver_rep_x = [diver_pos_x, diver_pos_x]
-    diver_rep_y = [diver_pos_y, diver_pos_y]
+    diver_rep_x = [diver_pos_x+31, diver_pos_x+94]
+    diver_rep_y = [diver_pos_y+14, diver_pos_y+20]
 
 #plastic movement
 def update_plastic_pos():
@@ -100,31 +100,34 @@ def enforce_border():
     if diver_pos_x > screen_width - diver_width:
         diver_pos_x = screen_width - diver_width
 
+
 #the function still can't work
 def check_for_score():
     global score
     if plastic_rep_x in range(diver_rep_x[0], diver_rep_x[1]) and plastic_rep_y in range(diver_rep_y[0], diver_rep_y[1]): 
-        score += 1
+        score = score + 1
         ting.play() #sound effect
     elif plastic_rep_y in range(diver_rep_y[0], diver_rep_y[1]) and plastic_rep_y not in range(diver_rep_x[0], diver_rep_x[1]): 
         score = 0
 
+
 #display score
 def show_score():
-    score_disp = font.render("SCORE: " + str(score), True, (0,0,0))
+    score_disp = font.render("SCORE: " + str(score), True, (0, 0, 0))
     screen.blit(score_disp, (text_x, text_y))
-    print(score)
+    # print(score)
+
 
 #main loop
 while play:
-    clock.tick(FPS)
+    clock.tick()
     screen.blit(court, (0,0))
     check_for_event() 
     update_plastic_pos() #plastic movement
     enforce_border() 
     show_images()
     check_for_score()
-    # show_score()
+    show_score()
     pygame.display.flip()
 
 pygame.quit()
